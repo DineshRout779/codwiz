e99b711e5c6335c547ca37078e1e65ab576577d0
Dinesh Rout <rddinesh779@gmail.com>
2023-04-26 02:37:17 +0530

    UI: added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import '../styles/navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <h2>Quizy</h2>
+
+      <div className='nav__profile'>
+        <h4>Hi, Dinesh</h4>
+        <div className='nav__profile__avatar'>
+          <img src='https://cdn.hashnode.com/res/hashnode/image/upload/v1627565932767/FNQ1u6YbV.png?w=200&h=200&fit=crop&crop=faces&auto=compress,format&format=webp' />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
