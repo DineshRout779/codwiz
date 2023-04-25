9acd8d7af017a2249a09a0ae64d10e1526e885bc
Dinesh Rout <rddinesh779@gmail.com>
2023-04-26 02:34:55 +0530

    feat: added data 🗃

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const questions = [
+  {
+    question: 'What is the result of the following code: 5 + "5"?',
+    options: ['0', '1', '10', '55'],
+    answer: 3,
+  },
+  {
+    question: 'Which of the following is not a JavaScript data type?',
+    options: ['String', 'Boolean', 'Float', 'Undefined'],
+    answer: 2,
+  },
+  {
+    question: 'What does the "this" keyword refer to in JavaScript?',
+    options: [
+      'The global object',
+      'The object that the function is a method of',
+      'The object that the function was called on',
+      'The object that the function is defined in',
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      'What does the following expression evaluate to: [1, 2, 3].slice(1)?',
+    options: ['[1, 2, 3]', '[1]', '[2, 3]', '[3]'],
+    answer: 2,
+  },
+  {
+    question: 'Which of the following is not a JavaScript loop statement?',
+    options: ['for', 'while', 'do-while', 'switch'],
+    answer: 3,
+  },
+];
