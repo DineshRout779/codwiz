aaeddd1874b1b83198469a4ec12ca5285fea8251
Dinesh Rout <rddinesh779@gmail.com>
2023-04-28 22:21:14 +0530

    Promt user to change language when questions not available

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,32 @@
 import { useApp } from '../context/AppContext';
 import '../styles/quiz.scss';
 import Question from './Question';
+import QuestionsNotFound from './QuestionsNotFound';
 import Score from './Score';
+import Status from './Status';
 
 function Quiz() {
   const { state } = useApp();
   const { currentQuestionIndex, questions } = state;
 
+  if (questions.length === 0) {
+    return <QuestionsNotFound />;
+  }
+
   return (
-    <>{currentQuestionIndex < questions.length ? <Question /> : <Score />}</>
+    <>
+      {currentQuestionIndex < questions.length ? (
+        <>
+          <Status />
+          <Question />
+        </>
+      ) : (
+        <>
+          <Status />
+          <Score />
+        </>
+      )}
+    </>
   );
 }
 
