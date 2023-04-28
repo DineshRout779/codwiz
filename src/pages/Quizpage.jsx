7aa3721ab112d210ee529888f8016565c4d67107
Dinesh Rout <rddinesh779@gmail.com>
2023-04-28 22:22:07 +0530

    throw to details page if user not found

--- src/pages/Quizpage.jsx
+++ src/pages/Quizpage.jsx
@@ -1,7 +1,21 @@
 import Body from '../components/Body';
 import Navbar from '../components/Navbar';
+import { useApp } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Quizpage = () => {
+  const {
+    state: { user },
+  } = useApp();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate(`/details`);
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -9,4 +23,5 @@ const Quizpage = () => {
     </>
   );
 };
+
 export default Quizpage;
